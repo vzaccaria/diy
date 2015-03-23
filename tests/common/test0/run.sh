d523b1e034b5e14288a08debad4f066fbd048c10
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-03-23 14:05:31 +0100

    Fix scripts with shellcheck

--- tests/common/test0/run.sh
+++ tests/common/test0/run.sh
@@ -3,16 +3,15 @@ set -e
 
 # Source directory
 #
-srcdir=`dirname $0`
-srcdir=`cd $srcdir; pwd`
-dstdir=`pwd`
+srcdir=$(dirname "$0")
+srcdir=$(cd "$srcdir"; pwd)
 
 bindir=$srcdir/../../..
 npm=$bindir/node_modules/.bin
 
-cd $srcdir
-rm -f $srcdir/makefile
-rm -f $srcdir/output
-node $srcdir/test0.js
-mv $srcdir/makefile $srcdir/output
-$npm/diff-files -m "Test compile, map and reduce" $srcdir/output $srcdir/reference
+cd "$srcdir"
+rm -f "$srcdir/makefile"
+rm -f "$srcdir/output"
+node "$srcdir/test0.js"
+mv "$srcdir/makefile" "$srcdir/output"
+"$npm/diff-files" -m "Test compile, map and reduce" "$srcdir/output" "$srcdir/reference"
